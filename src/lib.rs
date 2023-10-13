9bec5d18ffa1099edcc224a9b8b283a0dfe43b2f
Joe Fioti <jafioti@gmail.com>
2023-10-13 12:34:53 -0500

    Upgraded sycamore

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-#![allow(clippy::type_complexity)]
-use std::{fmt::Debug, marker::PhantomData};
+use std::fmt::Debug;
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use sycamore::{futures::spawn_local_scoped, prelude::*};
@@ -76,8 +75,8 @@ impl Toast {
 }
 
 #[derive(Default, Debug, Clone)]
-pub struct Toasts<T: Clone + Debug + Default + Serialize + DeserializeOwned> {
-    toasts: RcSignal<Vec<(T, u8)>>,
+pub struct Toasts<T: Clone + Debug + Default + Serialize + DeserializeOwned + 'static> {
+    toasts: Signal<Vec<(T, u8)>>,
 }
 
 pub enum CookieError {
@@ -89,7 +88,7 @@ impl<T: Clone + Debug + Default + Serialize + DeserializeOwned> Toasts<T> {
     pub fn from_cookies() -> Result<Self, CookieError> {
         if let Some(Ok(c)) = wasm_cookies::get("sycamore_toasts") {
             Ok(Self {
-                toasts: create_rc_signal(
+                toasts: create_signal(
                     serde_json::from_str(&c).map_err(|_| CookieError::InvalidCookie)?,
                 ),
             })
@@ -99,23 +98,24 @@ impl<T: Clone + Debug + Default + Serialize + DeserializeOwned> Toasts<T> {
     }
 
     pub fn clear_toasts(&self) {
-        self.toasts.modify().retain(|(_, r)| *r >= 1);
-        for (_, rank) in self.toasts.modify().iter_mut() {
-            *rank -= 1;
-        }
+        self.toasts.update(|i| i.retain(|(_, r)| *r >= 1));
+        self.toasts
+            .update(|i| i.iter_mut().for_each(|(_, i)| *i -= 1));
         self.save_to_cookies();
     }
 
     pub fn add_toast(&self, toast: T) -> &Self {
-        self.toasts.modify().push((toast, 0));
+        self.toasts.update(|i| i.push((toast, 0)));
         self.save_to_cookies();
         self
     }
 
     pub fn with_rank(&self, rank: u8) -> &Self {
-        if let Some((_, r)) = self.toasts.modify().last_mut() {
-            *r = rank;
-        }
+        self.toasts.update(|i| {
+            if let Some((_, r)) = i.last_mut() {
+                *r = rank;
+            }
+        });
         self.save_to_cookies();
         self
     }
@@ -124,91 +124,73 @@ impl<T: Clone + Debug + Default + Serialize + DeserializeOwned> Toasts<T> {
         // Save to cookies
         wasm_cookies::set(
             "sycamore_toasts",
-            &serde_json::to_string(self.toasts.get_untracked().as_ref()).unwrap(),
+            &serde_json::to_string(&self.toasts.get_clone_untracked()).unwrap(),
             &CookieOptions::default(),
         );
     }
 }
 
-#[derive(Prop, Default)]
-pub struct ToastsViewProp<
-    'a,
-    G: GenericNode,
-    F,
-    T: Clone + Debug + Default + Serialize + DeserializeOwned,
-> where
-    F: Fn(BoundedScope<'_, 'a>, T) -> View<G> + 'a,
-{
-    view: F,
-    toasts: Toasts<T>,
-    #[builder(default)]
-    _phantom: PhantomData<&'a ()>,
-}
-
-#[component]
+#[component(inline_props)]
 pub fn ToastsView<
-    'a,
     G: Html,
     T: Clone + Debug + Default + PartialEq + Serialize + DeserializeOwned + 'static,
-    F: Fn(BoundedScope<'_, 'a>, T) -> View<G> + 'a,
+    F: Fn(T) -> View<G> + 'static,
 >(
-    cx: Scope<'a>,
-    ToastsViewProp {
-        view,
-        toasts,
-        _phantom,
-    }: ToastsViewProp<'a, G, F, T>,
+    view: F,
+    toasts: Toasts<T>,
 ) -> View<G> {
-    if try_use_context::<Toasts<T>>(cx).is_none() {
-        provide_context(cx, toasts.clone());
+    if try_use_context::<Toasts<T>>().is_none() {
+        provide_context(toasts.clone());
     }
-    let new_toasts = create_memo(cx, move || {
+    let new_toasts = create_memo(move || {
         toasts
             .toasts
-            .get()
+            .get_clone()
             .iter()
             .filter(|(_, r)| *r == 0)
             .cloned()
             .map(|(t, _)| t)
             .collect()
     });
-    view! {cx,
+    view! {
         div (class="-translate-y-[300px] z-50") // To include the right class for fading out toasts
         div (class="fixed top-14 flex flex-col items-center z-50", style="left: 50%; width: 500px; max-width: 100vw; transform: translateX(-50%);") {
             Indexed (
                 iterable=new_toasts,
-                view=move |cx, toast| (view)(cx, toast)
+                view=view
             )
         }
     }
 }
 
 #[component]
-pub fn DefaultToastView<G: Html>(cx: BoundedScope, toast: Toast) -> View<G> {
+pub fn DefaultToastView<G: Html>(toast: Toast) -> View<G> {
     let toast1 = toast.clone();
-    let node_ref = create_node_ref(cx);
+    let node_ref = create_node_ref();
     let remove = move |_| {
         let toast1 = toast1.clone();
-        spawn_local_scoped(cx, async move {
+        spawn_local_scoped(async move {
             // Move to top
             node_ref.get::<DomNode>().add_class("-translate-y-[300px]");
             gloo_timers::future::TimeoutFuture::new(200).await;
             // Remove
-            let toasts = use_context::<Toasts<Toast>>(cx);
-            toasts.toasts.modify().retain(|(t, _)| t.id != toast1.id);
+            let toasts = use_context::<Toasts<Toast>>();
+            toasts
+                .toasts
+                .update(|i| i.retain(|(t, _)| t.id != toast1.id));
         })
     };
 
     // Spawn process to remove toast after 5 seconds
     let toast1 = toast.clone();
-    spawn_local_scoped(cx, async move {
+    spawn_local_scoped(async move {
         gloo_timers::future::TimeoutFuture::new(5000).await;
         // Move to top
         node_ref.get::<DomNode>().add_class("-translate-y-[300px]");
         gloo_timers::future::TimeoutFuture::new(200).await;
         // Remove
-        let toasts = use_context::<Toasts<Toast>>(cx);
-        toasts.toasts.modify().retain(|(t, _)| *t != toast1);
+        let toasts = use_context::<Toasts<Toast>>();
+        toasts.toasts.update(|i| i.retain(|(t, _)| *t != toast1));
     });
 
     let (bg_color, image_name) = match toast.toast_type {
@@ -217,7 +199,7 @@ pub fn DefaultToastView<G: Html>(cx: BoundedScope, toast: Toast) -> View<G> {
         ToastType::Primary => ("#395cfa", "info_toast.png"),
         ToastType::Success => ("#04c55e", "check_toast.png"),
     };
-    view! {cx,
+    view! {
         div (ref=node_ref, style=format!("border-color: {}", bg_color), class="w-full bg-white max-w-lg px-5 py-4 m-2 border-[3px] rounded-xl flex flex-row items-center transition-all z-50") {
             // Icon
             img (src=(format!("/static/images/icons/{image_name}")), width="30px", height="30px", class="object-scale-down")
@@ -225,12 +207,12 @@ pub fn DefaultToastView<G: Html>(cx: BoundedScope, toast: Toast) -> View<G> {
             // Title / text
             (if toast.body.replace(' ', "").is_empty() {
                 let title = toast.title.clone();
-                view!{cx,
+                view!{
                     p (class=(format!("ml-8 pt-1 font-montserrat text-lg font-bold text-slate-900"))) {(title)}
                 }
             } else {
                 let (title, message) = (toast.title.clone(), toast.body.clone());
-                view!{cx,
+                view!{
                     div (class="ml-8 flex flex-col") {
                         p (class=(format!("pt-1 font-montserrat text-lg font-bold text-slate-900"))) {(title)}
                         p (class="text-slate-700") {(message)}
@@ -254,9 +236,9 @@ mod tests {
 
     #[test]
     fn test_toasts() {
-        let _ = sycamore::render_to_string(|cx| {
+        let _ = sycamore::render_to_string(|| {
             let toasts = Toasts::default();
-            view! {cx,
+            view! {
                 ToastsView (
                     toasts=toasts,
                     view=DefaultToastView
